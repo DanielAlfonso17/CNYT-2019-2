b24ea3bc10dc629b7789fe8eb731295c3f428bc7
DanielAlfonso17 <54114923+DanielAlfonso17@users.noreply.github.com>
2019-09-13 23:33:59 -0500

    Add files via upload

--- Laboratorio 2/Laboratorio2.py
+++ Laboratorio 2/Laboratorio2.py
@@ -22,6 +22,14 @@ def productoImaginarios(c1,c2):
     res[1] = c3[1] + c3[2]
     return (res[0],res[1])
 
+def restaImaginarios(c1,c2):
+    c3 = [0,0]
+    uno = (-1*c2[0])
+    dos = (-1*c2[1])
+    c3[0] = c1[0] + uno
+    c3[1] = c1[1] + dos
+    return (c3[0],c3[1])
+
 def conjugadoImaginario(c1):
     c3 = [0,0]
     c3[0] = c1[0]
@@ -29,99 +37,59 @@ def conjugadoImaginario(c1):
     return (c3[0],c3[1])
 
 def inversoImaginario(c1):
-    conj = conjugadoImaginario(c1)
-    a = conj
-    b = (c1[0]**2) + (c1[1]**2)
-    resA = round((a[0]/b),2)
-    resB = round((a[1]/b),2)
+    resA = (-1*c1[0])
+    resB = (-1*c1[1])
     return (resA,resB)
 
+def conjugadoImaginario(c1):
+    c3 = [0,0]
+    c3[0] = c1[0]
+    c3[1] = (-1*c1[1])
+    return (c3[0],c3[1])
 
 """
 Funciones Laboratorio 2
 """
 
-
+#(1) Suma de vectores complejos 
 def adicionVectoresImaginarios(c1,c2):
     res = [] 
     for i in range(len(c1)):
         res.append(sumaImaginarios(c1[i],c2[i]))
-    return res 
+    return res
 
+#(2)Inverso aditivo de vector complejo 
 def inversaVectoresImaginarios(c1):
     res = []
     for i in range(len(c1)):
         res.append(inversoImaginario(c1[i]))
     return res
 
-def productoVectoresImaginarios(c1,c2):
-    ini = (0,0)
-    for i in range(len(c1)):
-        suma = productoImaginarios(c1[i],c2[i])
-        ini = sumaImaginarios(ini,suma)
-    return ini
-        
+#(3) Multiplicacion de escalar por vector complejo 
+def multiplicacionEscalarVector(c1,v1):
+    res = []
+    for i in range(len(v1)):
+        res.append(productoImaginarios(c1,v1[i]))
+    return res
+
+#(4) Suma de matrices complejos 
 def adicionMatricesImaginarias(m1,m2):
     matriz=[]
     for i in range(0,len(m1)):
         vector=[]
-        for j in range(0,len(m2)):
-            vector.append((m1[i][j][0]+m2[i][j][0],m1[i][j][1]+m2[i][j][1]))
+        for j in range(0,len(m2[i])):
+            vector.append(sumaImaginarios(m1[i][j],m2[i][j]))
         matriz.append(vector)
     return matriz
 
+#(5) Inverso aditivo de matriz compleja
 def inversaMatrizImaginaria(m1):
     matriz = []
     for i in range(0,len(m1)):
         matriz.append(inversaVectoresImaginarios(m1[i]))
     return matriz
 
-
-def productoMatricesImaginarias(m1,m2):
-    matriz = [[None] * len(m2[0]) for i in range(len(m1))]
-    for i in range(len(m1)):
-        for j in range(len(m2[0])):
-            columna = [row[j] for row in m2]
-            matriz[i][j] = productoVectoresImaginarios(m1[i],columna)
-    return matriz
-
-def matrizTranspuesta(m1):
-    matriz=[]
-    for i in range(0,len(m1[0])):
-        columna = [row[i] for row in m1]
-        matriz.append(columna)
-    return matriz 
-        
-        
-def matrizConjugada(m1):
-    matriz=[]
-    for i in range(0,len(m1)):
-        vector=[]
-        for j in range(0,len(m1[0])):
-            vector.append((m1[i][j][0],m1[i][j][1]*-1))
-        matriz.append(vector)
-    return matriz
-
-def matrizAdjunta(m1):
-    matriz=matrizTranspuesta(m1)
-    matriz=matrizConjugada(matriz)
-    return matriz
-
-def accionMatrizSobreVector(v1,m1):
-    v = []
-    for i in range(len(m1)):    
-            v.append(productoVectoresImaginarios(v1,m1[i]))
-    return v
-
-def inversaMatriz(m1):
-    matriz=[]
-    for i in range(0,len(m1)):
-        vector=[]
-        for j in range(0,len(m1[0])):
-            vector.append((m1[i][j][0]*-1,m1[i][j][1]*-1))
-        matriz.append(vector)
-    return matriz
-
+#(6) Multipliacion espacalar por matriz compleja 
 def complejoPorMatriz(c1,m1):
     matriz=[]
     for i in range(0,len(m1)):
@@ -131,6 +99,98 @@ def complejoPorMatriz(c1,m1):
         matriz.append(vector)
     return matriz
 
+#(7) Transpuesta de matriz compleja
+def matrizTranspuesta(m1):
+    matriz=[]
+    for i in range(0,len(m1[0])):
+        columna = [row[i] for row in m1]
+        matriz.append(columna)
+    return matriz 
+        
+#(8) Conjugada de matriz compleja        
+def matrizConjugada(m1):
+    matriz=[]
+    for i in range(0,len(m1)):
+        vector=[]
+        for j in range(0,len(m1[0])):
+            vector.append((m1[i][j][0],m1[i][j][1]*-1))
+        matriz.append(vector)
+    return matriz
+#(9) Adjunta (daga) de matriz compleja
+
+def matrizAdjunta(m1):
+    matriz=matrizTranspuesta(m1)
+    matriz=matrizConjugada(matriz)
+    return matriz
+
+#(10) Producto de matrices complejas
+def productoMatricesImaginarias(m1,m2):
+    matriz = [[None] * len(m2[0]) for i in range(len(m1))]
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            columna = [row[j] for row in m2]
+            matriz[i][j] = productoVectoresImaginarios(m1[i],columna)
+    return matriz
+#(11) Acción de matriz compleja sobre vector complejo
+def accionMatrizSobreVector(v1,m1):
+    v = []
+    for i in range(len(m1)):    
+            v.append(productoVectoresImaginarios(v1,m1[i]))
+    return v
+
+#(12) Producto interno de vectores complejos
+
+def productoInternoVectoresImaginarios(c1,c2):
+    ini = (0,0)
+    for i in range(len(c1)):
+        c3 = conjugadoImaginario(c1[i])
+        suma = productoImaginarios(c3,c2[i])
+        ini = sumaImaginarios(ini,suma)
+    return ini
+
+
+#(13) Norma de vector complejo
+def normaVectorImaginario(v1):
+    suma =0
+    for i in range(len(v1)):
+        for j in range(len(v1[0])):
+            suma += ((v1[i][j])**2)
+    return suma**(1/2)
+
+#(14) Distancia entre dos vectores complejos
+def distanciaDosVectoresImaginarios(v1,v2):
+    suma = 0
+    for i in range(len(v1)):
+        for j in range(len(v1[i])):
+            resta =v2[i][j]-v1[i][j]
+            suma += resta**2
+    return suma**(1/2)
+#(15) ¿Es la matriz compleja una matriz unitaria?
+def matrizUnitaria(m1):
+    esUnitaria = False
+    MatrizId=[[(1,0) if j == i else (0,0) for j in range(len(m1))] for i in range(len(m1))]
+    for i in range(len(m1)):
+        if(len(m1)!=len(m1[i])):
+            return "La matriz debe tener el mismo numero de filas y columnas"        
+
+    if(productoMatricesImaginarias(m1,matrizAdjunta(m1))==MatrizId):
+        esUnitaria = True
+    return esUnitaria
+
+#(16) ¿Es la matriz compleja una matriz hermitiana?
+def matrizHermitiana(m1):
+    m1T = matrizAdjunta(m1)
+    if m1T == m1:
+        return True
+    return False
+
+def productoVectoresImaginarios(c1,c2):
+    ini = (0,0)
+    for i in range(len(c1)):
+        suma = productoImaginarios(c1[i],c2[i])
+        ini = sumaImaginarios(ini,suma)
+    return ini
+#(17) Producto tensorial de matrices complejas  
 def productoTensorialImaginario(m1,m2):
     matriz = []
     for i in range(len(m1)):
--- Laboratorio 2/test_Laboratorio2.py
+++ Laboratorio 2/test_Laboratorio2.py
@@ -2,40 +2,120 @@ import unittest
 import Laboratorio2
 class TestUM(unittest.TestCase):
 
+    #(1) Suma de vectores complejos 
+    def test_caso_Adicion_Vectores_PMinima(self):
+        self.assertEqual([(16,0),(1,1),(3,-9)],Laboratorio2.adicionVectoresImaginarios([(8,3),(-1,-4),(0,-9)],[(8,-3),(2,5),(3,0)]))
+    #(1) Suma de vectores complejos 
     def test_caso_Adicion_Vectores_I(self):
         self.assertEqual([(22,-1.7),(7,-4),(10.2,-8.1),(0,-7)],Laboratorio2.adicionVectoresImaginarios([(6,-4),(7,3),(4.2,-8.1),(0,-3)],[(16,2.3),(0,-7),(6,0),(0,-4)]))
-
+    #(2)Inverso aditivo de vector complejo 
+    def test_caso_Inversa_Vectores_PMinima(self):
+        self.assertEqual([(5,-2),(-3,0),(0,1)],Laboratorio2.inversaVectoresImaginarios([(-5,2),(3,0),(0,-1)]))
+    #(2)Inverso aditivo de vector complejo 
     def test_caso_Inversa_Vectores_I(self):
-        self.assertEqual([(0.5,-0.5),(0.15,0.23),(-0.12,-0.47)],Laboratorio2.inversaVectoresImaginarios([(1,1),(2,-3),(-0.5,2)]))
-
+        self.assertEqual([(-1,-1),(-2,3),(5,-2)],Laboratorio2.inversaVectoresImaginarios([(1,1),(2,-3),(-5,2)]))
+    
     def test_caso_Producto_Vectores_I(self):
-        self.assertEqual((0,20),Laboratorio2.productoVectoresImaginarios([(1,1),(2,2),(3,3)],[(3,3),(2,2),(1,1)]))
-    
+            self.assertEqual((0,20),Laboratorio2.productoVectoresImaginarios([(1,1),(2,2),(3,3)],[(3,3),(2,2),(1,1)]))
+    #(3) Multiplicacion de escalar por vector complejo 
+    def test_caso_Producto_Escalar_Por_Vector_PMinima(self):
+        self.assertEqual([(-3,-7),(2,0),(7,11)],Laboratorio2.multiplicacionEscalarVector((-1,1),[(-2,5),(-1,-1),(2,-9)]))
+    #(4) Suma de matrices complejos         
     def test_caso_sumaMatriz_1(self):
-        self.assertEqual([[(8, -4), (11, 0)], [(3, 7), (-9, -7)]],Laboratorio2.adicionMatricesImaginarias([[[3,2],[4,-2]],[[1,2],[3,2]]],[[[5,-6],[7,2]],[[2,5],[-12,-9]]]))    
-    
+        self.assertEqual([[(8, -4), (11, 0)], [(3, 7), (-9, -7)]],Laboratorio2.adicionMatricesImaginarias([[[3,2],[4,-2]],[[1,2],[3,2]]],[[[5,-6],[7,2]],[[2,5],[-12,-9]]]))
+    #(4) Suma de matrices complejos         
+    def test_caso_sumaMatriz_PMinima(self):
+        self.assertEqual([[(-15, -5), (-10, -6),(7,3)], [(4, 17), (6, -7),(14,-10)],[(5,5),(2,-1),(-2,-1)]],Laboratorio2.adicionMatricesImaginarias([[(-8, -3), (-6, -4),(0,-4)], [(-1, 8), (6, -10),(8,-5)],[(4,0),(8,5),(-7,-9)]],[[(-7, -2), (-4, -2),(7,7)], [(5, 9), (0, 3),(6,-5)],[(1,5),(-6,-6),(5,8)]]))
+    #(5) Inverso aditivo de matriz compleja
     def test_caso_Inversa_Matriz_I(self):
-        self.assertEqual([[(0.2, -0.4), (0.25, -0.25)], [(0.17, -0.17), (0.1, -0.1)]],Laboratorio2.inversaMatrizImaginaria([[(1,2),(2,2)],[(3,3),(5,5)]]))
-        
-    def test_caso_Productro_Matrices_I(self):
-        self.assertEqual([[(9,6), (5,-4)], [(10, 30), (0,0)],[(4,1),(0,1)]],Laboratorio2.productoMatricesImaginarias([[(4,5),(1,0)],[(0,0),(6,-2)],[(-1,0),(0,-1)]],[[(1,-1),(0,-1)],[(0,5),(0,0)]]))
-
+        self.assertEqual([[(0.2, -0.4), (0.25, -0.25)], [(0.17, -0.17), (0.1, -0.1)]],Laboratorio2.inversaMatrizImaginaria([[(-0.2,0.4),(-0.25,0.25)],[(-0.17,0.17),(-0.1,0.1)]]))
+    #(5) Inverso aditivo de matriz compleja
+    def test_caso_Inversa_Matriz_PMinima(self):
+        self.assertEqual([[(-7, -3), (1, -7)], [(9, 4), (7, 9)]],Laboratorio2.inversaMatrizImaginaria([[(7,3),(-1,7)],[(-9,-4),(-7,-9)]]))
+    #(6) Multipliacion espacalar por matriz compleja
+    def test_caso_Complejo_por_Matriz(self):
+        self.assertEqual([[(0,13),(-4,32)],[(22,32),(28,10)]],Laboratorio2.complejoPorMatriz((-2,3),[[(3,-2),(8,-4)],[(4,-10),(-2,-8)]]))
+    #(7) Transpuesta de matriz compleja
     def test_caso_Matriz_Transpuesta(self):
         self.assertEqual([[(4,5), (0,0),(-1,0)], [(1,0),(6, -2),(0,-1)]],Laboratorio2.matrizTranspuesta([[(4,5),(1,0)],[(0,0),(6,-2)],[(-1,0),(0,-1)]]))
-
+    #(7) Transpuesta de matriz compleja
+    def test_caso_Matriz_Transpuesta_PMinima(self):
+        self.assertEqual([[(5,9), (8,2)], [(-7,-5),(-3, -7)],[(-1,-4),(7,-8)]],Laboratorio2.matrizTranspuesta([[(5,9),(-7,-5),(-1,-4)],[(8,2),(-3,-7),(7,-8)]]))
+    #(8) Conjugada de matriz compleja        
     def test_caso_Matriz_Conjugada(self):
         self.assertEqual([[(4,-5), (1,0)], [(0,0),(6,2)],[(-1,0),(0,1)]],Laboratorio2.matrizConjugada([[(4,5),(1,0)],[(0,0),(6,-2)],[(-1,0),(0,-1)]]))    
-
+    #(8) Conjugada de matriz compleja        
+    def test_caso_Matriz_Conjugada_PMinima(self):
+        self.assertEqual([[(-6,-1), (3,-8)], [(2,6),(3,0)]],Laboratorio2.matrizConjugada([[(-6,1),(3,8)],[(2,-6),(3,0)]]))
+    #(9) Adjunta (daga) de matriz compleja
     def test_caso_Matriz_Adjunta(self):
         self.assertEqual([[(4,-5), (0,0),(-1,0)], [(1,0),(6, 2),(0,1)]],Laboratorio2.matrizAdjunta([[(4,5),(1,0)],[(0,0),(6,-2)],[(-1,0),(0,-1)]]))
+    #(9) Adjunta (daga) de matriz compleja
+    def test_caso_Matriz_Adjunta_PMinima(self):
+        self.assertEqual([[(7,-7), (5,0)], [(3,-8),(8, 6)],[(8,-4),(-10,1)]],Laboratorio2.matrizAdjunta([[(7,7),(3,8),(8,4)],[(5,0),(8,-6),(-10,-1)]]))
+    #(10) Producto de matrices complejas
+    def test_caso_Productro_Matrices_I(self):
+        self.assertEqual([[(9,6), (5,-4)], [(10, 30), (0,0)],[(4,1),(0,1)]],Laboratorio2.productoMatricesImaginarias([[(4,5),(1,0)],[(0,0),(6,-2)],[(-1,0),(0,-1)]],[[(1,-1),(0,-1)],[(0,5),(0,0)]]))
+    #(10) Producto de matrices complejas
+    def test_caso_Productro_Matrices_I_PMinima(self):
+        self.assertEqual([[(-33,153), (120,0),(-44,-22)], [(87,0), (-26,-117),(107,70)],[(0,165),(147,26),(69,-36)]],Laboratorio2.productoMatricesImaginarias([[(-6,2),(0,6),(7,2)],[(6,9),(7,7),(-6,-6)],[(5,8),(-6,8),(6,9)]],[[(9,-6),(-3,-4),(5,-2)],[(3,6),(-1,-5),(0,-5)],[(9,9),(8,-4),(-8,-4)]]))
+    #(11) Acción de matriz compleja sobre vector complejo
+    def test_caso_Accion_Matriz_Vector(self):
+        self.assertEqual([(9, -3), (-15, 0)],Laboratorio2.accionMatrizSobreVector([(-3,1),(5,0)],[[(2,0),(3,-1)],[(3,1),(-1,0)]]))
 
+    #(11) Acción de matriz compleja sobre vector complejo
+    def test_caso_Accion_Matriz_Vector(self):
+        self.assertEqual([(9, -3), (-15, 0)],Laboratorio2.accionMatrizSobreVector([(-3,1),(5,0)],[[(2,0),(3,-1)],[(3,1),(-1,0)]]))
+
+    #(11) Acción de matriz compleja sobre vector complejo
+    def test_caso_Accion_Matriz_Vector_PMinima(self):
+        self.assertEqual([(54, -32), (0, 17),(41,30)],Laboratorio2.accionMatrizSobreVector([(1,-3),(4,3),(-3,1)],[[(-1,5),(1,-7),(-6,3)],[(-3,-9),(2,-5),(1,-10)],[(-6,5),(6,-5),(3,-2)]]))
+
+    #(12) Producto interno de vectores complejos
+    def test_caso_Producto_Interno_Vector_PMinima(self):
+        self.assertEqual((4,1),Laboratorio2.productoInternoVectoresImaginarios([(2,-1),(-8,-5),(-2,-6)],[(6,-3),(5,-1),(-6,-2)]))
+
+    #(13) Norma de vector complejo
+    def test_caso_Norma_Vectores_Imaginarios_PMinima(self):
+        self.assertEqual(10,Laboratorio2.normaVectorImaginario([(4,5),(3,1),(0,-7)]))
+
+    #(14) Distancia entre dos vectores complejos
+    def test_caso_Distancia_entre_Vectores_PMinima(self):
+        self.assertEqual(12,Laboratorio2.distanciaDosVectoresImaginarios([(2,7),(4,-1),(2,-4)],[(7,8),(2,-8),(1,4)]))
+
+    #(15) ¿Es la matriz compleja una matriz unitaria?
+    def test_caso_Matriz_Unitaria_PMinima(self):
+        self.assertEqual(True,Laboratorio2.matrizUnitaria([[((1/(2**(1/2))),0),(0,(1/(2**(1/2))))],[(0,(1/(2**(1/2)))),((1/(2**(1/2))),0)]]))
+
+    #(15) ¿Es la matriz compleja una matriz unitaria?
+    def test_caso_Matriz_Unitaria_PMinima1(self):
+        self.assertEqual(False,Laboratorio2.matrizUnitaria([[(0,1),(1,0),(0,0)],[(0,0),(0,1),(1,0)],[(1,0),(0,0),(0,1)]]))
+    
+
+    #(16) ¿Es la matriz compleja una matriz hermitiana?
+    def test_caso_Matriz_Hermitiana(self):
+        self.assertEqual(True,Laboratorio2.matrizHermitiana([[(2,0),(3,-1)],[(3,1),(-1,0)]]))
+        
+    #(16) ¿Es la matriz compleja una matriz hermitiana?
+    def test_caso_Matriz_Hermitiana_PMinima(self):
+        self.assertEqual(True,Laboratorio2.matrizHermitiana([[(3,0),(2,-1),(0,-3)],[(2,1),(0,0),(1,-1)],[(0,3),(1,1),(0,0)]]))
+
+    #(16) ¿Es la matriz compleja una matriz hermitiana?
+    def test_caso_Matriz_Hermitiana_PMinima1(self):
+        self.assertEqual(False,Laboratorio2.matrizHermitiana([[(1,0),(3,-1)],[(3,1),(0,1)]]))
+
+    #(17) Producto tensorial de matrices complejas    
     def test_caso_ProductoTensor(self):
         m=[[(1,0),(2,0)],[(3,0),(4,0)]]
         m1=[[(1,0),(2,0),(3,0)],[(1,0),(2,0),(3,0)],[(1,0),(2,0),(3,0)]]
         self.assertEqual([[(1, 0), (2, 0), (3, 0), (2, 0), (4, 0), (6, 0)], [(1, 0), (2, 0), (3, 0), (2, 0), (4, 0), (6, 0)], [(1, 0), (2, 0), (3, 0), (2, 0), (4, 0), (6, 0)], [(3, 0), (6, 0), (9, 0), (4, 0), (8, 0), (12, 0)], [(3, 0), (6, 0), (9, 0), (4, 0), (8, 0), (12, 0)], [(3, 0), (6, 0), (9, 0), (4, 0), (8, 0), (12, 0)]],Laboratorio2.productoTensorialImaginario(m,m1))
 
-    def test_caso_Accion_Matriz_Vector(self):
-        self.assertEqual([(9, -3), (-15, 0)],Laboratorio2.accionMatrizSobreVector([(-3,1),(5,0)],[[(2,0),(3,-1)],[(3,1),(-1,0)]]))
+    #(17) Producto tensorial de matrices complejas    
+    def test_caso_ProductoTensor_PMinima(self):
+        m=[[(1,1),(0,0)],[(1,0),(0,1)]]
+        m1=[[(-1,2),(-2,-2),(0,2)],[(2,3),(3,1),(2,2)],[(-2,1),(1,-1),(2,1)]]
+        self.assertEqual([[(-3, 1), (0, -4), (-2, 2), (0, 0), (0, 0), (0, 0)], [(-1, 5), (2, 4), (0, 4), (0, 0), (0, 0), (0, 0)], [(-3, -1), (2, 0), (1, 3), (0, 0), (0, 0), (0, 0)], [(-1, 2), (-2, -2), (0, 2), (-2, -1), (2, -2), (-2, 0)], [(2, 3), (3, 1), (2, 2), (-3, 2), (-1, 3), (-2, 2)], [(-2, 1), (1, -1), (2, 1), (-1, -2), (1, 1), (-1, 2)]],Laboratorio2.productoTensorialImaginario(m,m1))
+    
         
 
 if __name__ =='__main__':
